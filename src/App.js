78bdfc07f6097d7da7423cee77379e80d709f047
Khang Nguyễn Vĩ <vikhang17112002@gmail.com>
2024-03-18 01:05:05 +0700

    Login screen, register/confirm screen, forgotpassword/confirm screen

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import HomeScreen from "./pages/HomeScreen/HomeScreen";
 import LoginScreen from "./pages/LoginScreen/LoginScreen";
+import RegisterScreen from './pages/RegisterScreen/RegisterScreen'
+import RegisterConfirmScreen from './pages/RegisterConfirmScreen/RegisterConfirmScreen'
+import ForgotPasswordScreen from "./pages/ForgotPassword/ForgotPasswordScreen";
+import ForgotPasswordConfirm from "./pages/ForgotPasswordConfirm/ForgotPasswordConfirm";
 
 function App() {
     return (
         <Routes>
             <Route path={'/'} element={<HomeScreen/>}/>
             <Route path={'/login'} element={<LoginScreen/>}/>
+            <Route path={'/register'} element={<RegisterScreen/>}/>
+            <Route path={'/register-confirm'} element={<RegisterConfirmScreen/>}/>
+            <Route path={'/forgot-password'} element={<ForgotPasswordScreen/>}/>
+            <Route path={'/forgot-password-confirm'} element={<ForgotPasswordConfirm/>}/>
         </Routes>
     );
 }
